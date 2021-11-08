1b373e6a08b17a5c713e10734b659c9572c5ea25
tanmach2 <phamtan160499@gmail.com>
2021-11-08 16:26:23 +0700

    change springboot

--- authentication_with_jwt/src/main/java/com/example/authentication_with_jwt/custom_exception/UserIdNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.example.authentication_with_jwt.custom_exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class UserIdNotFound extends AuthenticationException {
-    public UserIdNotFound(String msg) {
-        super(msg);
-    }
-}
--- authentication_with_jwt/src/main/java/com/example/authentication_with_jwt/repositories/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.authentication_with_jwt.repositories;
-
-import com.example.authentication_with_jwt.entities.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Integer> {
-    Address findByWard(String ward);
-}
